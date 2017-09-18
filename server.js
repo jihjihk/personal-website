5cd62648cc8f3521ea4ed80fe3f25ac3442abaa6
jihjihk <jihyun@nyu.edu>
2017-09-19 00:06:26 +0400

    poc website

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var path = require('path');
+var app = express()
+var fs = require('fs');
+var firstline = require('firstline');
+var time = new Date();
+var allWriting = require('./public/text/writing-posts.json');
+var allProjects = require('./public/text/project-posts.json');
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "public/views"));
+
+app.listen(8005, function() {
+	console.log("We have started the server");
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, err) {
+	res.render('index.pug');
+});
+
+app.get('/writing', function(req, res, err) {
+	res.render('writing.pug', { allWriting: allWriting });
+});
+
+app.get('/projects', function(req, res, err) {
+	res.render('projects.pug', { allProjects: allProjects });
+})
+
+app.get('/about', function(req, res, err) {
+	res.render('about.pug');
+})
+
+app.get('/writing/:week', function(req, res, err) {
+	var content = {
+		title: '',
+		subtitle: '',
+		week: '',
+		text:'',
+		date:''
+	};
+
+	content = allWriting[req.params.week]
+	res.render('writing-template', content);
+})
+
+app.get('/projects/:week', function(req, res, err) {
+	var content = {
+		title: '',
+		subtitle: '',
+		week: '',
+		text:'',
+		image: '',
+		date:''
+	};
+
+	content = allProjects[req.params.week]
+	res.render('project-template', content);
+})
